b44e7f9fac8a0e0bcad2c63461aa1e4b238ffe5c
Bruno Pimentel <bpimentel@mastertech.tech>
2018-08-28 08:50:34 -0300

    Lê a lista e carrega para memória

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 const fs = require('fs');
-
 const comando = process.argv[2];
 const entrada = process.argv[3];
-const listaTarefas = [];
+let listaTarefas = [];
 
-if(comando === 'inserir'){
-    let tarefa = {
-        conteudo: entrada,
-        feita: false
+fs.readFile('tarefas.json', {encoding: 'UTF-8'}, function(erro, dados){
+    listaTarefas = JSON.parse(dados);
+    
+    if(comando === 'inserir'){
+        let tarefa = {
+            conteudo: entrada,
+            feita: false
+        }
+        
+        listaTarefas.push(tarefa);
+        
+        let listaTarefasString = JSON.stringify(listaTarefas);
+        
+        fs.writeFile('tarefas.json', listaTarefasString, function(erro){
+            console.log(erro);
+        });
+    } else if(comando === 'listar'){
+        console.log('O comando foi listar');
+    } else if(comando === 'fazer'){
+        console.log('O comando foi fazer');
+    } else if(comando === 'limpar'){
+        console.log('O comando foi limpar');
+    } else {
+        console.log('Comando inválido.');
     }
-
-    listaTarefas.push(tarefa);
-
-    let listaTarefasString = JSON.stringify(listaTarefas);
-
-    fs.writeFile('tarefas.json', listaTarefasString, function(erro){
-        console.log(erro);
-    });
-} else if(comando === 'listar'){
-    console.log('O comando foi listar');
-} else if(comando === 'fazer'){
-    console.log('O comando foi fazer');
-} else if(comando === 'limpar'){
-    console.log('O comando foi limpar');
-} else {
-    console.log('Comando inválido.');
-}
+});
